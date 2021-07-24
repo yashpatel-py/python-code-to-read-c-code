7c3207742231c609014ce1f8c9b3934fd74612d6
yash-2115 <mailforyash21@gmail.com>
2021-07-24 13:29:31 +0530

    first commit

---
+++ c_code_input.c
@@ -0,0 +1,10 @@
+// Lexical Analysis
+/* Program */
+#include <stdio.h> // This is a header file
+int main()
+{
+    int a,b,c,d;
+    a=10;
+    printf("The value of a is %d ",a);
+    return 0;
+}
